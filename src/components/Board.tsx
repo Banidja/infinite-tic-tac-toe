3c859641835526d0c934e7b4dd1e9b975e69889a
Branislav Jaksic <branislavjaksic3@gmail.com>
2024-09-09 17:41:28 +0200

    fix: allow three moves for "0"

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -20,6 +20,35 @@ const Board = () => {
 
   const { increaseScore, restartScore } = useScoreStore();
 
+  const handleMoveOrder = (
+    newSquares: Array<XAndO | null>,
+    player: XAndO
+  ): void => {
+    if (moveOrder.length === 7) {
+      const indexToRemove = moveOrder.findIndex(
+        (ind) => squares[ind] === player
+      );
+
+      if (indexToRemove !== -1) {
+        newSquares[moveOrder[indexToRemove]] = null;
+        moveOrder.splice(indexToRemove, 1);
+        setSquares(newSquares);
+      }
+    } else setSquares(newSquares);
+  };
+
+  const handleGameModes = (
+    newSquares: Array<XAndO | null>,
+    player: XAndO,
+    index: number
+  ): void => {
+    if (gameMode === "resetMode") setSquares(resetFilledLines(newSquares));
+    else {
+      moveOrder.push(index);
+      handleMoveOrder(newSquares, player);
+    }
+  };
+
   const handleSquareClick = (index: number): void => {
     if (squares[index]) return;
 
@@ -32,37 +61,6 @@ const Board = () => {
     setIsXNext(!isXNext);
 
     if (calculateWinner(newSquares)) handleReset(currentPlayer);
-  };
-
-  const handleGameModes = (
-    newSquares: Array<XAndO | null>,
-    player: XAndO,
-    index: number
-  ): void => {
-    if (gameMode === "resetMode") {
-      setSquares(resetFilledLines(newSquares));
-    } else {
-      moveOrder.push(index);
-      handleMoveOrder(newSquares, player);
-    }
-  };
-
-  const handleMoveOrder = (
-    newSquares: Array<XAndO | null>,
-    player: XAndO
-  ): void => {
-    if (moveOrder.length === 6) {
-      const indexToRemove = moveOrder.findIndex(
-        (ind) => squares[ind] === player
-      );
-      if (indexToRemove !== -1) {
-        newSquares[moveOrder[indexToRemove]] = null;
-        moveOrder.splice(indexToRemove, 1);
-        setSquares(newSquares);
-      }
-    } else {
-      setSquares(newSquares);
-    }
   };
 
   const handleReset = (player?: XAndO): void => {
