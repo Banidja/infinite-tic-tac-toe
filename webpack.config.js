f023920402134e9c91077f3dc01d26900903d46a
Branislav Jaksic <branislavjaksic3@gmail.com>
2024-11-01 16:56:36 +0100

    feat: add webpack

--- webpack.config.js
+++ webpack.config.js
@@ -36,7 +36,7 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['css-loader', 'postcss-loader'],
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
       }
     ],
   },
